4b6fa4ade7ab7303f6ed7dc8632b445ef98174fd
Cory Petkovsek <632766+tinmanjuggernaut@users.noreply.github.com>
2019-11-12 16:05:14 +0800

    mount files into docker for android

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ $podman build --build-arg mono_version=${mono_version} -t godot-mono-glue:latest
 $podman build --build-arg mono_version=${mono_version} -v $(pwd)/files:/root/files -t godot-windows:latest -f Dockerfile.windows .
 $podman build --build-arg mono_version=${mono_version} -t godot-ubuntu-32:latest -f Dockerfile.ubuntu-32 .
 $podman build --build-arg mono_version=${mono_version} -t godot-ubuntu-64:latest -f Dockerfile.ubuntu-64 .
-$podman build --build-arg mono_version=${mono_version} -t godot-android:latest -f Dockerfile.android .
+$podman build --build-arg mono_version=${mono_version} -v $(pwd)/files:/root/files -t godot-android:latest -f Dockerfile.android .
 
 $podman build -t godot-javascript:latest -f Dockerfile.javascript .
 
