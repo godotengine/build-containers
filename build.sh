64ee6284b5ce0bb6f532959e9e8ebc36f595bd6e
Rémi Verschelde <remi@godotengine.org>
2021-03-27 12:04:01 +0100

    OSX/iOS: Update to Xcode 12.4 and latest osxcross
    Osxcross update should improve things for Apple Silicon, notably we
    no longer need to disable LDID fake signing, it was replaced by
    adhoc-codesign.

--- build.sh
+++ build.sh
@@ -120,9 +120,9 @@ $podman_build_mono -t godot-ubuntu-32:${img_version} -f Dockerfile.ubuntu-32 . 2
 $podman_build_mono -t godot-javascript:${img_version} -f Dockerfile.javascript . 2>&1 | tee logs/javascript.log
 $podman_build_mono -t godot-android:${img_version} -f Dockerfile.android . 2>&1 | tee logs/android.log
 
-XCODE_SDK=12.2
-OSX_SDK=11.0
-IOS_SDK=14.2
+XCODE_SDK=12.4
+OSX_SDK=11.1
+IOS_SDK=14.4
 if [ ! -e files/MacOSX${OSX_SDK}.sdk.tar.xz ] || [ ! -e files/iPhoneOS${IOS_SDK}.sdk.tar.xz ] || [ ! -e files/iPhoneSimulator${IOS_SDK}.sdk.tar.xz ]; then
   if [ ! -e files/Xcode_${XCODE_SDK}.xip ]; then
     echo "files/Xcode_${XCODE_SDK}.xip is required. It can be downloaded from https://developer.apple.com/download/more/ with a valid apple ID."
